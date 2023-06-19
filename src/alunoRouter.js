c7e887fb46c646ff743dbe72e720616ce5b22051
Jeremias :) <apf1@aluno.ifal.edu.br>
2023-06-19 13:22:04 -0300

    MANDANDO

--- src/alunoRouter.js
+++ src/alunoRouter.js
@@ -3,28 +3,44 @@ const Aluno = require('./alunoModel');
 const router = express.Router();
 router.use(express.json());
 
-router.get('/', (req, res) => {
-    res.send('Get all')
+router.get('/', async (req, res) => {
+    const alunos = await Aluno.findAll();
+    res.send(alunos);
 })
 
-router.get('/:id', (req, res) => {
-    res.send(req.params.id)
+router.get('/:id', async (req, res) => {
+    const alunos = await Aluno.findByPk(req.params.id);
+    res.send(alunos);
 })
 
-router.get('/matricula/:matricula', (req, res) => {
-    res.send(req.params.matricula)
+router.get('/matricula/:matricula', async (req, res) => {
+    const aluno = await Aluno.findOne({
+        where: {
+            matricula: req.params.matricula
+        }
+    })
+    res.send(aluno);
 })
 
-router.post('/', (req, res) => {
-    res.send(req.body)
+router.post('/', async (req, res) => { //cria e salva
+    let aluno = await Aluno.create(req.body);
+    res.send(aluno);
 })
 
-router.put('/:id', (req, res) => {
-    res.send(req)
+router.put('/:id', async (req, res) => { //atualiza
+    let aluno = await Aluno.findByPk(req.params.id);
+    if(aluno){
+        aluno.matricula = req.body.matricula;
+        aluno = await aluno.save(aluno);
+    }
+    res.send(aluno);
 })
 
-router.delete('/:id', (req, res) => {
-    res.status(204).send(req)
+router.delete('/:id', async (req, res) => { //deleta
+    let aluno = await Aluno.findByPk(req.params.id);
+    aluno.destroy();
+
+    res.status(204).send();
 })
 
-module.exports = router
+module.exports = router;
